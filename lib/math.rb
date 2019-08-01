e88fe94411522f27e208ddad8246a86666af7b23
Christopher Sexton <cpsexton@gmail.com>
2019-08-01 19:39:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,5 @@
 def addition(num1, num2)
-num1 + num2
+  num1 + num2
 end
 
 def subtraction(num1, num2)
@@ -7,17 +7,18 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
 
 def modulo(num1, num2)
-
+  Math.num1 / num2
+  
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
